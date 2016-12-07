0a1957b124d56abbf8410f49a63e655a612a80e8
DESKTOP-VH57QE2\biasi <biasiol.silvio@gmail.com>
2016-12-07 18:11:42 +0100

    added landing page link in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
+/**
 app.use('/', function(request, response) 
 {
 	var text = 'Hello world';
@@ -13,7 +13,21 @@ app.use('/', function(request, response)
     response.end(text);
   	
 });
+**/
 
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
