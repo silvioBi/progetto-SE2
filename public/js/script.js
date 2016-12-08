1104ae80bdddabaec0bf96ba4954c62f1ed48701
DESKTOP-VH57QE2\biasi <biasiol.silvio@gmail.com>
2016-12-08 14:45:04 +0100

    finally fixed bug of menus

--- public/js/script.js
+++ public/js/script.js
@@ -1,45 +1,90 @@
+var visible = [false,false,false,false];
+
 $(document).ready(function(){
-    $(".menu_button").click(function(){
-        $("#landing").toggle();
-        $("#bottom-image").fadeToggle();
-    });
     
     $(".menu_button.btn0").click(function(){
-        $(".panel.orange").slideToggle();
-        $(".panel.green").slideUp();
-        $(".panel.red").slideUp();
-        $(".panel.turquese").slideUp();
-        
-        if ($('.panel.green').is(':hidden') ||
-           $('.panel.red').is(':hidden') || 
-           $('.panel.orange').is(':hidden') || 
-           $('.panel.turquese').is(':hidden')){
-            if ($('#landing').is(':hidden')) {
-                $("#landing").toggle();
-                $("#bottom-image").fadeToggle();
+        $(".panel.orange").slideToggle("medium");
+        visible[0]=!visible[0];
+        if (visible[0]) {
+            for (i=1;i<visible.length;i++) {
+                visible[i]=false;
             }
         }
+        $(".panel.green").fadeOut("fast");
+        $(".panel.red").fadeOut("fast");
+        $(".panel.turquese").fadeOut("fast");
+        hide_show_welcome_message()
     });
     
     $(".menu_button.btn1").click(function(){
-        $(".panel.green").slideToggle();
-        $(".panel.turquese").slideUp();
-        $(".panel.red").slideUp();
-        $(".panel.orange").slideUp();
+        $(".panel.green").slideToggle("medium");
+        visible[1]=!visible[1];
+        if (visible[1]) {
+            for (i=0;i<visible.length && i!=1;i++) {
+                visible[i]=false;
+            }
+        }
+        $(".panel.turquese").fadeOut("fast");
+        $(".panel.red").fadeOut("fast");
+        $(".panel.orange").fadeOut("fast");
+        hide_show_welcome_message()
     });
     
     $(".menu_button.btn2").click(function(){
-        $(".panel.turquese").slideToggle();
-        $(".panel.green").slideUp();
-        $(".panel.red").slideUp();
-        $(".panel.orange").slideUp();
+        $(".panel.turquese").slideToggle("medium");
+        visible[2]=!visible[2];
+        if (visible[2]) {
+            for (i=0;i<visible.length && i!=2;i++) {
+                visible[i]=false;
+            }
+        }
+        $(".panel.green").fadeOut("fast");
+        $(".panel.red").fadeOut("fast");
+        $(".panel.orange").fadeOut("fast");
+        hide_show_welcome_message()
     });
     
     $(".menu_button.btn3").click(function(){
-        $(".panel.red").slideToggle();
-        $(".panel.green").slideUp();
-        $(".panel.turquese").slideUp();
-        $(".panel.orange").slideUp();
+        $(".panel.red").slideToggle("medium");
+        visible[3]=!visible[3];
+        if (visible[3]) {
+            for (i=0;i<visible.length && i!=3;i++) {
+                visible[i]=false;
+            }
+        }
+        $(".panel.green").fadeOut("fast");
+        $(".panel.turquese").fadeOut("fast");
+        $(".panel.orange").fadeOut("fast");
+        hide_show_welcome_message()
     });
     
 });
+
+function hide_show_welcome_message() {
+    var show = true;
+    for (i=0;i<visible.length;i++) {
+        if (visible[i]) {
+            show = !show;
+        }
+    }
+    if (show) {
+        $("#landing").fadeIn();
+        $("#bottom-image").fadeIn();
+    } else {
+        $("#landing").fadeOut();
+        $("#bottom-image").fadeOut();
+    }
+    
+    /*
+    console.log($('.panel.orange').is(':visible'));
+  if ($('.panel.green').is(':hidden') &&
+      $('.panel.red').is(':hidden') && 
+      $('.panel.orange').is(':hidden') && 
+      $('.panel.turquese').is(':hidden')) {
+        $("#landing").fadeIn();
+        $("#bottom-image").fadeIn();
+  } else {
+        $("#landing").fadeOut();
+        $("#bottom-image").fadeOut();
+  }  */
+}
